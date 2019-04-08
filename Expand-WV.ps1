d8ef37306026cd20610bd9b04de97839327de90b
chalstead <chalstead@vmware.com>
2019-04-08 17:44:36 -0400

    Update Expand-WV.ps1
    Cleaned up

--- Expand-WV.ps1
+++ Expand-WV.ps1
@@ -6,7 +6,7 @@
   Parameters Below
 
 .OUTPUTS
-  Log file stored in C:\Windows\Temp\<name>.log>
+  Log file stored in %temp%\expand-wv.log>
 
 .NOTES
   Version:        1.0
@@ -90,34 +90,33 @@ Function Write-Log {
     [CmdletBinding()]
     Param(
     
-    [Parameter(Mandatory=$False)]
-    [ValidateSet("INFO","WARN","ERROR","FATAL","DEBUG")]
-    [String]
-    $Level = "INFO",
-
     [Parameter(Mandatory=$True)]
-    [string]
+    [System.Object]
     $Message
 
     )
-    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $Stamp = (Get-Date).toString("MM/dd/yyyy HH:mm:ss")
     $Line = "$Stamp $Level $Message"
     Add-Content $sLogFile -Value $Line
    
     }
 
 Function Connect_AV {
-
-Write-Host "Logging on to App Volumes Manager"
+#Connect to App Volumes Manager
+Write-Host "Logging on to App Volumes Manager: $appvolumesserverfqdn"
 try{$sresult = Invoke-RestMethod -Method Post -Uri "https://$AppVolumesServerFQDN/cv_api/sessions" -Body $Credentials -SessionVariable avsession}
 catch {
   Write-Host "An error occurred when logging on $_"
-  Add-Content $sLogFile -Value "Error when logging on to AppVolumes Manager: $_"
-  Add-Content $sLogFile -Value "Finishing Script******************************************************"
+  Write-Log -Message "Error when logging on to AppVolumes Manager: $_"
+  Write-Log -Message "Finishing Script*************************************"
   exit 
 }
 
-Add-Content $sLogFile -Value "Logging on to AppVolumes Manager: $sresult"
+write-Log -Message "Logging on to AppVolumes Manager: $sresult"
+write-Log -Message "Enumerating Writable Volumes"
+Write-Host "Enumerating Writable Volumes"
+
+#Get Writable Volumes
 $sgetwv = Invoke-RestMethod -WebSession $avsession -Method Get -Uri "https://$AppVolumesServerFQDN/cv_api/writables" -ContentType 'application/json'
 
 $json = $sgetwv.datastores.writable_volumes
@@ -126,33 +125,38 @@ $json = $sgetwv.datastores.writable_volumes
     {
 
     If ($Update_WV_Size -eq "YES") {
-          
+       #Update Writable Volumes   
       $avid = $item.id
       try{$supdatesize = Invoke-RestMethod -WebSession $avsession -Method Post -Uri "https://$AppVolumesServerFQDN/cv_api/writables/grow?bg=0&size_mb=$New_Size_In_MB&volumes%5B%5D=$avid" -ContentType 'application/json'}
       catch {
         Write-Host "An error occurred when increasing size $_"
       }
       if ($supdatesize.successes.Count -gt 0)
-        {Write-Host $supdatesize.successes}
+        {Write-Host $supdatesize.successes
+        Write-Log -Message $supdatesize.successes
+        }
       if ($supdatesize.warnings.Count -gt 0)
-        {Write-Host $supdatesize.warnings}
+        {Write-Host $supdatesize.warnings
+        Write-Log -Message $supdatesize.warnings
+        }
       if ($supdatesize.errors.Count -gt 0)
-        {Write-Host $supdatesize.errors}
+        {Write-Host $supdatesize.errors
+        Write-Log -Message $supdatesize.errors.Item(0)     
+        }
        }   else {
      
-        Write-Host $item.id $item.name $item.total_mb $item.attached 
-    
+        Write-Host $item.name $item.total_mb $item.percent_available $item.attached 
+        Add-Content -path $sLogFile -value "$item.name $item.total_mb $item.percent_available $item.attached"
         }
 
     }
    
 
-
   } 
 
 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
-#Logon to App Volumes
-Connect_AV
 
-Add-Content $sLogFile -Value "Finishing Script******************************************************"
+Connect_AV
+Write-Log -Message "Finishing Script******************************************************"
+Write-Host "Finished"
