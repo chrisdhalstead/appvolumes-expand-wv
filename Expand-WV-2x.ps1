edb7d8521bd5f5e61da6d56af3d829eef85a7a05
Chris Halstead <chalstead@vmware.com>
2022-02-22 11:53:23 -0500

    Ignore Certs

--- Expand-WV-2x.ps1
+++ Expand-WV-2x.ps1
@@ -71,6 +71,20 @@
 
 #----------------------------------------------------------[Declarations]----------------------------------------------------------
 #Log File Info
+
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
 $sLogPath = $env:TEMP 
 $sDomain = $env:USERDOMAIN
 $sUser = $env:USERNAME
